8867975ea5b6d4c77dd3f36e1ddeeff328b6e568
Cristian Camilo Vasquez <cvo6372@gmail.com>
2026-09-06 18:00:04 -0500

    Enhance cron job management by integrating user role validation. Update CronJobsService to check for admin roles before executing scheduled tasks. Introduce AdminGuard in CronController to restrict access to admin users only. Update CronModule to include UsersLoginRepository for user role management.

--- src/cron/infrastructure/modules/cron.module.ts
+++ src/cron/infrastructure/modules/cron.module.ts
@@ -10,8 +10,18 @@ import {
   CronLogModel,
   CronLogSchema,
 } from "src/shared/infrastructure/mongo/schemas/cron-log.schema";
+import {
+  StatusModel,
+  StatusSchema,
+} from "src/shared/infrastructure/mongo/schemas/status.schema";
+import {
+  UserModel,
+  UserSchema,
+} from "src/shared/infrastructure/mongo/schemas/user.schema";
+import { AdminGuard } from "src/shared/infrastructure/guards/admin.guard";
 import { JwtAuthGuard } from "src/shared/infrastructure/guards/jwt-auth.guard";
 import { JwtStrategy } from "src/shared/infrastructure/strategies/jwt.strategy";
+import { UsersLoginMongoImplementation } from "src/users/infrastructure/implementations/mongo/users-login-mongo.implementation";
 import { CronJobsService } from "src/cron/application/cron.service";
 import { CronController } from "../controllers/cron.controller";
 
@@ -21,6 +31,8 @@ import { CronController } from "../controllers/cron.controller";
     MongooseModule.forFeature([
       { name: CronConfigModel.name, schema: CronConfigSchema },
       { name: CronLogModel.name, schema: CronLogSchema },
+      { name: UserModel.name, schema: UserSchema },
+      { name: StatusModel.name, schema: StatusSchema },
     ]),
     JwtModule.register({
       secret: process.env.JWT_SECRET || "secretKey",
@@ -28,6 +40,15 @@ import { CronController } from "../controllers/cron.controller";
     }),
   ],
   controllers: [CronController],
-  providers: [JwtStrategy, JwtAuthGuard, CronJobsService],
+  providers: [
+    JwtStrategy,
+    JwtAuthGuard,
+    AdminGuard,
+    CronJobsService,
+    {
+      provide: "UsersLoginRepository",
+      useClass: UsersLoginMongoImplementation,
+    },
+  ],
 })
 export class CronModule {}
